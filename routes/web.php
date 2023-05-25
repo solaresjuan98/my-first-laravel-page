25494bac1e2a334001f150a8127df6a618ef7349
Juan Antonio Solares <solaresjuan43@gmail.com>
2023-05-25 11:08:58 -0600

    laravel templates

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 
 Route::get('blog', function () {
@@ -26,11 +26,11 @@ Route::get('blog', function () {
     ];
 
     return view('blog', ['posts' => $posts]);
-});
+})->name('blog');
 
 Route::get('blog/{slug}', function ($slug) {
 
     $post = $slug;
 
     return view('post', ['post' => $post]);
-});
+})->name('post');
