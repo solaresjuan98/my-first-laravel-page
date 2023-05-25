6ef353580bb7b23ec1334364e3a127ddb6025710
Juan Antonio Solares <solaresjuan43@gmail.com>
2023-05-25 10:13:58 -0600

    adding basic routes

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+
+Route::get('blog', function () {
+
+    $posts = [
+        ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel'],
+    ];
+
+    return view('blog', ['posts' => $posts]);
+});
+
+Route::get('blog/{slug}', function ($slug) {
+
+    $post = $slug;
+
+    return view('post', ['post' => $post]);
+});
