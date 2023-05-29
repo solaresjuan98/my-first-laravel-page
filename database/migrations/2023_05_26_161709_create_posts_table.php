38d6de14cd203bea039b389c6cffea58725a3912
Juan Antonio Solares <solaresjuan43@gmail.com>
2023-05-29 11:07:10 -0600

    working with migrations

--- database/migrations/2023_05_26_161709_create_posts_table.php
+++ database/migrations/2023_05_26_161709_create_posts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-
+            $table->text('title');
             $table->string('slug')->unique();
             $table->text('body');
 
