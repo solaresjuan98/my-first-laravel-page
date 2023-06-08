cbba16b5a021f9243874866bb23f6612c5de8bb1
Juan Antonio Solares <solaresjuan43@gmail.com>
2023-06-08 17:20:59 -0600

    adding posts list

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -10,7 +11,9 @@ class PostController extends Controller
 
     public function index() 
     {
-        return view('posts.index');
+        return view('posts.index', [
+            'posts' => Post::latest()->paginate()
+        ]);
     }
 
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,28 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    Posts lists
+                    
+                    <table class="mb-4">
+                        @foreach ($posts as $post)
+
+                        <tr class="border-b border-gray-200 text-sm">
+                            <td class="px-6 py-4">
+                                {{ $post->title }}
+                            </td>
+                            <td class="px-6 py-4">
+
+                                <a href="" class="text-indigo-600">Edit</a>
+                                
+                            </td>
+
+                            <td class="px-6 py-4">
+                                Delete
+                            </td>
+                        </tr>
+
+                        @endforeach
+                    </table>
+                    {{ $posts->links() }}
                 </div>
             </div>
         </div>
