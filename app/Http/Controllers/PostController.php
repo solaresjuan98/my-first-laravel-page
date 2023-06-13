4ab8c2b62c026eff6b86616d5ab9d91368003b3f
Juan Antonio Solares <solaresjuan43@gmail.com>
2023-06-13 16:21:18 -0600

    save post done

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -15,9 +16,23 @@ class PostController extends Controller
         ]);
     }
 
-    public function create()
+    public function create(Post $post)
     {
-        return view('posts.create');
+        return view('posts.create', ['post' => $post]);
+    }
+
+    
+    public function store(Request $request)
+    {
+        
+        $post = $request->user()->posts()->create(
+            [
+                'title' => $title = $request->title,
+                'slug' => Str::slug($title),
+                'body' => $request->body,
+            ]
+        );
+        return redirect()->route('posts.edit', $post);
     }
 
     public function edit(Post $post)
--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,1 +1,16 @@
-Form
+@csrf 
+
+<label class="uppercase text-gray-700 text-xs">Title</label>
+<input type="text" name="title" class="rounded border-gray-200 text-gray-900 w-full mb-4" value="{{ $post->title }}"></input>
+
+<label class="uppercase text-gray-700 text-xs">Content</label>
+<textarea name="body" rows="5" class="rounded border-gray-200 text-gray-900 w-full mb-4">{{ $post->body }}</textarea>
+
+<div class="flex justify-between items-center">
+
+    <a href="{{ route('posts.index') }}" class="text-indigo-600">Go back</a>
+
+
+    <input type="submit" value="Send" class="bg-red-600 text-white rounded px-4 py-2">
+
+</div>
