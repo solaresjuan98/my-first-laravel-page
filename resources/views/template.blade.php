0fc7c208811978e40e5e3381ed5c69ce05eb56b0
Juan Antonio Solares <solaresjuan43@gmail.com>
2023-07-15 15:53:02 -0600

    upload changes

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,26 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web project</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
 </head>
+
 <body>
-    <p>
-        <a href="{{ route('home') }}">Home</a>
-        <a href="{{ route('blog') }}">Blog</a>
-    </p>
+    <div class="container px-4 mx-auto">
 
-    @auth
-    <a href="{{ route('dashboard') }}">Dashboard</a>
-    @else
-    <a href="{{ route('login') }}">Dashboard</a>
-    @endauth
+        <header class="flex justify-between items-center py-4">
+            <div class="flex items-center flex-grow gap-4">
+                <a href="{{ route('home') }}">
+                    <img src="{{ asset('images/logo.png') }}" class="h-12">
 
-    <hr>
+                </a>
 
-    @yield('content')
+                <form action="">
+                    <input type="text" placeholder="search">
+                </form>
+            </div>
+
+            @auth
+            <a href="{{ route('dashboard') }}">Dashboard</a>
+            @else
+            <a href="{{ route('login') }}">Dashboard</a>
+            @endauth
+
+        </header>
+        @yield('content')
+    </div>
 
 </body>
+
 </html>
